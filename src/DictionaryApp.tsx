b8b1a527c399e70d30d73c8ce16969710ff71284
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-11 02:07:11 -0700

    Render data from api

--- src/DictionaryApp.tsx
+++ src/DictionaryApp.tsx
@@ -1,5 +1,21 @@
+import { useCallback, useEffect, useState } from 'react';
+import { dictionaryApi } from './api/dictionaryApi';
+import { DictionaryResponse } from './interfaces/interfaces';
+import { AxiosResponse } from 'axios';
+
 export const DictionaryApp = () => {
-  return (
-    <div>DictionaryApp</div>
-  )
-}
+	const [data, setData] = useState<DictionaryResponse>();
+	const fetchData = useCallback(async () => {
+		const response: AxiosResponse<DictionaryResponse, []> = await dictionaryApi.get('/dog');
+		setData(response.data);
+	}, []);
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
+  if(!data){
+    return <>Loading...</>
+  }
+	return <div>
+    {JSON.stringify(data,null,2)}
+  </div>;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), checker({ typescript: true /** or an object config */ })],
+	plugins: [
+		react(),
+		checker({
+			typescript: true,
+		}),
+	],
 });
