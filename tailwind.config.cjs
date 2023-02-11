f40496c1c7ebcfa458c62fffb33ab87458b16b52
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-11 01:19:44 -0700

    Color palette config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				VeryDark: '#050505',
+				Dark: '#1F1F1F',
+				LightDark: '#2D2D2D',
+				VeryLightDark: '#3A3A3A',
+				VeryGray: '#838383',
+				Gray: 'rgb(233,233,233)',
+				Purplish: 'rgb(164,69,237)',
+				SoftRed: 'rgb(255,82,82)',
+			},
+		},
+	},
+	plugins: [],
+};
