3b0530cd546682a81187ead1da6c6ccdf3185d78
JaacielBriseo <jaacielbris23@gmail.com>
2023-02-11 02:37:16 -0700

    Fetch data with hooks and debounced input

--- src/DictionaryApp.tsx
+++ src/DictionaryApp.tsx
@@ -1,21 +1,49 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, useRef, ChangeEvent } from 'react';
+import { AxiosResponse } from 'axios';
 import { dictionaryApi } from './api/dictionaryApi';
 import { DictionaryResponse } from './interfaces/interfaces';
-import { AxiosResponse } from 'axios';
 
 export const DictionaryApp = () => {
 	const [data, setData] = useState<DictionaryResponse>();
+	const [query, setQuery] = useState('');
+	const [inputValue, setInputValue] = useState('');
+	const debounceRef = useRef<ReturnType<typeof setTimeout>>();
+
 	const fetchData = useCallback(async () => {
-		const response: AxiosResponse<DictionaryResponse, []> = await dictionaryApi.get('/dog');
+		const response: AxiosResponse<DictionaryResponse, []> = await dictionaryApi.get(`/${query}`);
 		setData(response.data);
-	}, []);
+	}, [query]);
+
+	const onQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (debounceRef.current) clearTimeout(debounceRef.current);
+		debounceRef.current = setTimeout(() => {
+			setQuery(event.target.value);
+		}, 1000);
+	};
+
 	useEffect(() => {
+		if (query === '') return;
 		fetchData();
-	}, [fetchData]);
-  if(!data){
-    return <>Loading...</>
-  }
-	return <div>
-    {JSON.stringify(data,null,2)}
-  </div>;
+	}, [fetchData, query]);
+
+	return (
+		<>
+			<form className='flex flex-col justify-center items-center mt-20'>
+				<input
+					type='text'
+					name='query'
+					value={inputValue}
+					onChange={e => {
+						onQueryChange(e);
+						setInputValue(e.target.value);
+					}}
+					className='border-red-500 border-4 h-10 w-4/12'
+				/>
+				<button type='submit' className='bg-fuchsia-400 p-4'>
+					submit
+				</button>
+			</form>
+			<div>{JSON.stringify(data)}</div>
+		</>
+	);
 };
